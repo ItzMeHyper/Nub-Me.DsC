14acbfc626ffe24ec93bfbd6a281bea9dabf2537
Hyper <133499771+ItzMeHyper@users.noreply.github.com>
2024-08-29 20:06:20 +0530

    Update BubbleSort.c
    Enter the number of elements: 5
    Enter the Elemnts: 
    4
    9
    7
    5
    6
    The sorted elemts are: 
    4       5       6       7       9 
    Space complexity = 40
    Time complexity = 61

--- BubbleSort.c
+++ BubbleSort.c
@@ -1,32 +1,47 @@
 #include <stdio.h>
 
 void main() {
-    int i, j, n, temp;
+    int i, j, n, temp, time =0; 
+    time++;
 
     printf("Enter the number of elements: ");
+    time++;
     scanf("%d", &n);
+    time++;
 
     int arr[n]; 
 
     printf("Enter the Elemnts: \n");
+    time++;
 
     for(i = 0; i < n; i++) {
+        time++;
         scanf("%d", &arr[i]);
+        time++;
     }
 
     for(i = 0; i < n-1; i++) {
-
+        time++;
         for(j = 0; j < n-i-1; j++) {
-
+            time++;
             if(arr[j] > arr[j+1]) {
+                time++;
                 temp = arr[j];
+                time++;
                 arr[j] = arr[j+1];
+                time++;
                 arr[j+1] = temp;
+                time++;
             }
         }
     }
     printf("The sorted elemts are: \n");
+    time++;
     for(i = 0; i<n; i++){
+        time++;
         printf("%d \t", arr[i]);
+        time++;
     }
+    time++;time++;
+    printf("\nSpace complexity = %d\nTime complexity = %d\n", 20 + (4 * n), time);
 }
