f8be65e1dfb8e95a7bb68f7252dc6bd64dd1e1cc
Hyper <133499771+ItzMeHyper@users.noreply.github.com>
2024-09-09 20:27:07 +0530

    Update Queue.c
    Updated The Enqueue function

--- Queue.c
+++ Queue.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int Q[100],i, front = -1, rear = -1, MAX, dlt_value;
+int Q[100], i, front = -1, rear = -1, MAX, dlt;
 
-void enqueue(int value) {
+void enqueue() {
+    int value;
     if (rear == MAX - 1) {
         printf("Queue overflow\n");
-    } else {
+    }
+    else {
         if (front == -1) {
             front = 0;
         }
+        printf("Enter the value to be inserted: ");
+        scanf("%d", &value);
         rear++;
         Q[rear] = value;
         printf("%d inserted onto the Queue\n", value);
@@ -19,37 +23,40 @@ void enqueue(int value) {
 void dequeue() {
     if (front == -1) {
         printf("Queue underflow\n");
-    } else {
-        dlt_value = Q[front];
+    }
+    else {
+        dlt = Q[front];
         if (front == rear) {
             front = rear = -1;
-        } else {
+        }
+        else {
             front++;
         }
-        printf("%d removed from the queue\n", dlt_value);
+        printf("%d removed from the queue\n", dlt);
     }
 }
 
 void display() {
     if (front == -1) {
         printf("Queue is empty\n");
-    } else {
+    }
+    else {
         printf("Queue elements are:\n");
-        for (i = front; i <=rear; i++) {
+        for (i = front; i <= rear; i++) {
             printf("%d  ", Q[i]);
         }
-    printf("\n");
+        printf("\n");
     }
 }
 
 int main() {
-    int choice, value;
+    int choice;
 
     printf("Enter the queue size: ");
     scanf("%d", &MAX);
 
     while (1) {
-        printf("\n=================================\n");
+        printf("=================================\n");
         printf("         Queue Operations        \n");
         printf("=================================\n");
         printf("---------------\n");
@@ -62,23 +69,21 @@ int main() {
         scanf("%d", &choice);
 
         switch (choice) {
-            case 1:
-                printf("Enter the value to be inserted: ");
-                scanf("%d", &value);
-                enqueue(value);
-                break;
-            case 2:
-                dequeue();
-                break;
-            case 3:
-                display();
-                break;
-            case 4:
-                printf("\nExiting program. Goodbye!\n");
-                exit(0);
-                break;
-            default:
-                printf("Invalid choice, please try again.\n");
+        case 1:
+            enqueue();
+            break;
+        case 2:
+            dequeue();
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            printf("\nExiting program. Goodbye!\n");
+            exit(0);
+            break;
+        default:
+            printf("Invalid choice, please try again.\n");
         }
     }
     return 0;
